855b891bcc8289756b44cf17a3bb5623cb694335
Robert Smith <robert@stylewarning.com>
2024-07-04 12:48:26 -0700

    add emacs extra files

---
+++ emacs-extras/term/vt200.el
@@ -0,0 +1,10 @@
+;; get the things to work
+(define-key input-decode-map "\e[1;2A" [S-up])
+(define-key input-decode-map "\e[1;2B" [S-down])
+(define-key input-decode-map "\e[1;2C" [S-right])
+(define-key input-decode-map "\e[1;2D" [S-left])
+
+(define-key input-decode-map "\e[1;5A" [C-up])
+(define-key input-decode-map "\e[1;5B" [C-down])
+(define-key input-decode-map "\e[1;5C" [C-right])
+(define-key input-decode-map "\e[1;5D" [C-left])
